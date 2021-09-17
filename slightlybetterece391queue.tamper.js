eeb9f4c51b229bae5e783a13c289a281e726829d
Sahan Kumarasinghe <55329269+sahansk2@users.noreply.github.com>
2021-09-17 05:40:29 -0500

    New features: suppress the TA-specific notification, add notification when user is in red queue zone.

--- slightlybetterece391queue.tamper.js
+++ slightlybetterece391queue.tamper.js
@@ -1,11 +1,8 @@
 // ==UserScript==
 // @name         Slightly Enhanced ECE391 Queue
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  A REALLY hacky TamperMonkey script that I might not ever maintain. Ever. In fact, the course staff might
-// @description  update the site and break this script unintentionally (or intentionally), but that's totally OK -- you use this at your own risk.
-// @description  This script just adds queue position numbers, automatic number highlighting based
-// @description  on queue position, and automatic scrolling towards your own NetID in the queue.
+// @namespace    http://www.github.com/
+// @version      0.2
+// @description  See https://www.github.com/sahansk2/qol-tampermonkey
 // @author       Sahan Kumarasinghe
 // @match        http://ece391test.web.illinois.edu/*
 // @match        https://ece391test.web.illinois.edu/*
@@ -13,28 +10,11 @@
 // @grant        none
 // ==/UserScript==
 
-/*
-Copyright (C) 2021 Sahan Kumarasinghe
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
 
 (function() {
     'use strict';
 
-    // ==UserScript==
+// ==UserScript==
 // @name     Unnamed Script 523372
 // @version  1
 // @grant    none
@@ -42,12 +22,22 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 const getNetId = () => {
     let res = document.querySelector("#new_question input[name='netid']")?.value
-    console.debug('Extracted netId: ', res)
+    console.debug('Extracted netId:', res)
     if (res) return res;
     else return "";
 }
-const updatePos = () => {
-    console.debug('Entering updatePos')
+
+// The website is going to ask us for notification access anyway, so we just sit back and relax.
+const notifyImminentDequeue = () => {
+    if (Notification.permission === "granted") {
+        new Notification('ECE391 Queue', {
+            icon: 'https://courses.engr.illinois.edu/ece391/sp2019/images/logo.png',
+            body: "Expect to be dequeued very soon!",
+        });
+    }
+}
+
+const queuePositionUpdates = () => {
     let results = document.querySelectorAll('.question_card h5')
     let taCount = document.querySelectorAll('.ta_info_list > li').length
     console.debug('Number of online TAs', taCount)
@@ -56,12 +46,16 @@ const updatePos = () => {
     for (let i = 0; i < results.length; i++) {
         let counterStr = `${i+1}`.padStart(3, '0')
         let counterNode = results[i].querySelector('span')
-        let netId = results[i].querySelector('small').innerText
-        netId = netId.slice(1,netId.length-1) // Trim parens
+        let thisNetId = results[i].querySelector('small').innerText
+        thisNetId = thisNetId.slice(1,thisNetId.length-1)
+        let userNetId = getNetId()
         counterNode.innerText = counterStr
         if (i < taCount) {
             // Expect to be called soon
             counterNode.className += " badge-danger"
+            if (thisNetId === userNetId) {
+                notifyImminentDequeue()
+            }
         } else if (i >= taCount*2) {
             counterNode.className += " badge-secondary"
         } else {
@@ -87,7 +81,7 @@ const highlightSelf = () => {
 
 // Safari users won't get this, because Safari is the new IE.
 const scrollToOwnPosition = () => {
-    // Obviously this is only going to work if you
+    // Must be logged in for this.
     let netId = getNetId()
     if (netId) {
         document.querySelector(`a[netid="${netId}"]`)?.scrollIntoView({behavior: 'smooth'})
@@ -95,15 +89,20 @@ const scrollToOwnPosition = () => {
 }
 
 const updateCycle = () => {
-    updatePos()
+    queuePositionUpdates()
     scrollToOwnPosition()
 }
 
 const observer = new MutationObserver(updateCycle)
-let questions = document.querySelector('.question-group')
-if (questions) {
-    observer.observe(questions, {childList: true})
-    injectQueueCounter()
-    updateCycle()
-}
+
+observer.observe(document.querySelector('.question-group'), {childList: true})
+
+injectQueueCounter()
+
+updateCycle()
+
+const suppressTANotify = () => { console.debug("TA student-ready notification suppressed.") }
+
+window.notify = suppressTANotify
+
 })();
